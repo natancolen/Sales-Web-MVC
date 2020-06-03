505c680b42728dbfb10b928aa4307bc838c66c2e
Natã Batista Brandão <natancolen@gmail.com>
2020-06-03 13:06:14 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -10,6 +10,8 @@ namespace SalesWebMvc.Models {
             : base(options) {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet <Seller> Seller{ get; set; }
+        public DbSet <SalesRecord> SalesRecord { get; set; }
     }
 }
--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,6 +1,28 @@
-﻿namespace SalesWebMvc.Models {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models {
     public class Department {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() {
+
+        }
+
+        public Department(int id, string name) {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSaller(Seller seller) {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSalles(DateTime initial, DateTime final) {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
